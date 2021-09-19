f0dab182f4ecf1a0bad39694edd871281a8f9b8b
--replace-all <k435467@gmail.com>
2021-09-19 18:33:47 +0800

    add MUI

---
+++ next/lib/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+  },
+});
+
+export default theme;
