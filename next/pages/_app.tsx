ae7d38d3b5fa41fcb6fa333a8aff440fb4dd5687
--replace-all <k435467@gmail.com>
2021-09-20 17:34:57 +0800

    sessionProvider

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -6,6 +6,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import theme from "../lib/theme";
 import createEmotionCache from "../lib/createEmotionCache";
+import { Provider } from "next-auth/client";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -25,7 +26,28 @@ export default function MyApp(props: MyAppProps) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />
+        <Provider
+          // Provider options are not required but can be useful in situations where
+          // you have a short session maxAge time. Shown here with default values.
+          options={{
+            // Client Max Age controls how often the useSession in the client should
+            // contact the server to sync the session state. Value in seconds.
+            // e.g.
+            // * 0  - Disabled (always use cache value)
+            // * 60 - Sync session state with server if it's older than 60 seconds
+            clientMaxAge: 0,
+            // Keep Alive tells windows / tabs that are signed in to keep sending
+            // a keep alive request (which extends the current session expiry) to
+            // prevent sessions in open windows from expiring. Value in seconds.
+            //
+            // Note: If a session has expired when keep alive is triggered, all open
+            // windows / tabs will be updated to reflect the user is signed out.
+            keepAlive: 0,
+          }}
+          session={pageProps.session}
+        >
+          <Component {...pageProps} />
+        </Provider>
       </ThemeProvider>
     </CacheProvider>
   );
